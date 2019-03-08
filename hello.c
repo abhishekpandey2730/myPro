e9ba30a3948bbe7b6565c49b4ef1389386a4a292
Abhishek Pandey <abhishekpandey.edu.ap@gmail.com>
2019-03-08 17:07:13 +0530

    first commit

---
+++ hello.c
@@ -0,0 +1,15 @@
+LIST OF IDEAS
+
+1. app to manage ticketing of public transportation and to help users to travel from one place to another without using personal transport
+2. app to manage financials for gig economy and to handle donations also if required and subscription also.
+3. corporate travel management app-------------tripactions.com, travelperk.com
+4. app to buy all gift cards and corporate gift management app
+5. tool like cloudplex for aws, azure and gcp and swb like tool to automate end to end process.
+6. insurance app from where all types of insurance can be sold, by sales rep and also corporate accounts, payments can be tracked by users on app and all the benefits also
+7. app to raise money for ngo or any public works like gofundme.com
+8. something on the lines of substack.com
+9. getethos.com
+10. helpdesk ticketing service like askspoke.com
+11. develop a bloomberg like terminal on the lines of sentieo.com
+12. visitor mgmt app like envoy.com
+13. subscription billing software like zuora
